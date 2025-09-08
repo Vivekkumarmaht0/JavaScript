7ee1a70b3d2121dd4985a2c9195fe892b6dd0841
Vivek Kumar Mahto <vivekkumarmahto101103@gmail.com>
2025-09-08 02:12:33 +0000

    variables config commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 45624
+let accountEmail = "vb6333598@gmail.com"  // mostly we have to use let
+var accountPassword = "12345"             // bcoz we have want to change something it can change in the whole var same name variable
+accountCity = "Ranchi"
+let accountState                // undefined
+
+// accountId = 23  no allowed const variable can't changed
+
+accountEmail = "vivekkumar@gmail.com"
+accountPassword = "54321"
+accountCity = "Kakinada"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
