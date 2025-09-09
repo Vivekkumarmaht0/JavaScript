0aee17ff7ac5ab1f6b5eb46edc804ac9f3719131
Vivek Kumar Mahto <vivekkumarmahto101103@gmail.com>
2025-09-09 02:02:05 +0000

    Coversion & Operations config commit

---
+++ 01_basics/03_CoversionOperation.js
@@ -0,0 +1,45 @@
+let score = "Vivek"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)       // Number
+// console.log(valueInNumber)              // NaN
+
+
+// "33" --> 33
+// "33abc" --> NaN
+// true --> 1; false --> 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let number = 33
+
+let stringNumber = String(number)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
+
+// ***************************************** Operations ******************************
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+// Link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
