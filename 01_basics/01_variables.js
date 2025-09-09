57fc3977a8ce62c9509db8ea936a1cdd1724e636
Vivek Kumar Mahto <vivekkumarmahto101103@gmail.com>
2025-09-09 01:16:27 +0000

    Data Type config commit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -12,5 +12,10 @@ accountCity = "Kakinada"
 
 console.log(accountId);
 
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+// "use strict"     // treat all js code as newer version
+
+// alert(3 + 3)  we are using nodejs, not browser
+
+let name = "Vivek"
+let age = 18
+let isBoolean = false
+
+// Primitive DataType
+
+// number --> 2 to power 53
+// bigint  --> long numbers
+// string --> ""
+// boolean --> true/false
+// null --> standalone value     Type (Object)
+// undefined -->
+// symbol --> unique
+
+// Other DataType
+
+// Object
+
+console.log(typeof undefined)   // undefined
+console.log(typeof age)         // number
+console.log(typeof null)        // Object
