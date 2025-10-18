267a01a57f4a12bcc9da57f29aea199c0a5cbad2
Vivek Kumar Mahto <vivekkumarmahto101103@gmail.com>
2025-10-18 05:38:34 +0000

    Numbers and Maths

---
+++ 01_basics/NumbersAndMaths.js
@@ -0,0 +1,21 @@
+// Order of Operations and Brackets()
+// Brackets --> Multiplication and Division --> Additions and Substraction  
+// If same then starts from left side
+
+// console.log(20.99 + 10.8)
+// console.log((20.99 + 10.8 * 2) * 0.1)
+
+// console.log(Math.round(20.99 + 10.8 * 2) / 0.1) 
+// console.log(Math.round(2.8))
+
+console.log((1 * 10) + (3 * 8) + (1 * 5))
+console.log(((1 * 10) + (3 * 8) + (1 * 5)) / 3)
+
+console.log(18.50 + (2 * 7.50))
+console.log((18.50 + 2 * 7.50) * 0.1)
+console.log((18.50 + 2 * 7.50) * 0.2)
+
+console.log(Math.round((18.50 + 2 * 7.50) * 1.0))
+
+console.log(Math.ceil(2.2))
+console.log(Math.floor(2.8))
